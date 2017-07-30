990ff689bec576121e226c0248214b73cfc6bbe6
mohammedGC <30494953+mohammedGC@users.noreply.github.com>
2017-07-30 10:44:57 +0300

    new php mailing method

--- action_page.php
+++ action_page.php
@@ -1,43 +1,12 @@
+<?php
+$name    =  test_input($_POST["name"]);
+$email    =  test_input($_POST["email"]);
+$to      = 'tkotd18@gmail.com';
+$subject    =  test_input($_POST["subject"]);
+$message    =  test_input($_POST["message"]);
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
-$nameErr = $emailErr = $numberErr = $subjectErr = "";
-$name = $email = $comment = $number = $subject = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed"; 
-    }
-  }
-
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format"; 
-    }
-  }
-
-  if (empty($_POST["number"])) {
-    $number = "Number is required";
-  } else {
-    $number = test_input($_POST["number"]);
-  }
-
-  if (empty($_POST["comment"])) {
-    $comment = "";
-  } else {
-    $comment = test_input($_POST["comment"]);
-  }
-
-  if (empty($_POST["subject"])) {
-    $subjectErr = "Subject is required";
-  } else {
-    $subject = test_input($_POST["subject"]);
-  }
-}
+mail($to, $subject, $message, $headers);
+?> 
